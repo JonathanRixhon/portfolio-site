5a3161598f5b374b79c4fb280b773d9cfbdf4768
Jonathan Rixhon <jonathan@whitecube.be>
2024-01-01 21:27:14 +0100

    add responsive for cards

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -1,6 +1,8 @@
-<section class="about">
-    <h2 class="about__title">{{ $content['title'] }}</h2>
-    <div class="wysiwyg">
-        {!! $content['body'] !!}
+<section class="about wrapper">
+    <div class="about__container">
+        <h2 class="about__title">{{ $content['title'] }}</h2>
+        <div class="wysiwyg">
+            {!! $content['body'] !!}
+        </div>
     </div>
 </section>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <x-hero :content="$page->content['hero']" />
-    <x-featured-works :content="$page->content['works']"/>
+    <x-featured-works :content="$page->content['works']" />
+    <x-about :content="$page->content['about']" />
     {{--
-    <x-about :content="$page->content['about']"/>
     <x-skills :content="$page->content['skills']"/>
     <x-contact :content="$page->content['contact']"/> --}}
 @endsection
