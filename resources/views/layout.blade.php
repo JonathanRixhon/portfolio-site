3205f0161c958c020c3545c29466dbb2fad8d6b2
Jonathan Rixhon <jonathan@whitecube.be>
2024-01-01 23:35:53 +0100

    Fix body height

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -37,19 +37,6 @@
     </main>
 
     <x-footer />
-    <div class="gradient-effect" aria-hidden="true">
-        <svg class="gradient-effect__gradient gradient-effect__gradient-1" viewBox="0 0 517 1891" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <ellipse cx="258.498" cy="945.049" rx="258.498" ry="945.049" />
-        </svg>
-        <svg class="gradient-effect__gradient gradient-effect__gradient-2" viewBox="0 0 517 1891" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <ellipse cx="258.498" cy="945.049" rx="258.498" ry="945.049" />
-        </svg>
-        <svg class="gradient-effect__gradient gradient-effect__gradient-3" viewBox="0 0 517 1891" fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <ellipse cx="258.498" cy="945.049" rx="258.498" ry="945.049" />
-        </svg>
-    </div>
+    <x-gradient-effect />
 </body>
 </html>
