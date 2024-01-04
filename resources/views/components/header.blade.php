5afc733f70934934c77c876c2010e7672486caf5
Jonathan Rixhon <jonathan@whitecube.be>
2024-01-04 12:18:41 +0100

    fix width andspacings

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,23 +2,25 @@
     <h1 class="sro">{{ $page->getTitle() }}</h1>
     <nav class="nav wrapper">
         <h2 class="sro">Main Navigation</h2>
-        <a href="{{ route('home') }}" class="nav__link nav__link--home">Jonathan Rixhon</a>
-        <ul class="nav__items">
-            <li class="nav__item">
-                <a href="{{ route('home') }}" class="nav__link {{ request()->routeIs('home') ? 'nav__link--current' : ''}}">Home</a>
-            </li>
-            <li class="nav__item">
-                <a href="{{ route('works') }}" class="nav__link {{ request()->routeIs('works') ? 'nav__link--current' : ''}}">Works</a>
-            </li>
-            <li class="nav__item">
-                <a href="{{ route('contact') }}" class="nav__link {{ request()->routeIs('contact') ? 'nav__link--current' : ''}}" data-modal="contact-form">Contact</a>
-            </li>
-            {{-- <li class="nav__item nav__item--icon">
-                <a href="#" class="nav__link nav__link--icon">Linkedin</a>
-            </li>
-            <li class="nav__item nav__item--icon">
-                <a href="#" class="nav__link nav__link--icon" data-modal="contact-form">Github</a>
-            </li> --}}
-        </ul>
+        <div class="nav__content">
+            <a href="{{ route('home') }}" class="nav__link nav__link--home">Jonathan Rixhon</a>
+            <ul class="nav__items">
+                <li class="nav__item">
+                    <a href="{{ route('home') }}" class="nav__link {{ request()->routeIs('home') ? 'nav__link--current' : ''}}">Home</a>
+                </li>
+                <li class="nav__item">
+                    <a href="{{ route('works') }}" class="nav__link {{ request()->routeIs('works') ? 'nav__link--current' : ''}}">Works</a>
+                </li>
+                <li class="nav__item">
+                    <a href="{{ route('contact') }}" class="nav__link {{ request()->routeIs('contact') ? 'nav__link--current' : ''}}" data-modal="contact-form">Contact</a>
+                </li>
+                {{-- <li class="nav__item nav__item--icon">
+                    <a href="#" class="nav__link nav__link--icon">Linkedin</a>
+                </li>
+                <li class="nav__item nav__item--icon">
+                    <a href="#" class="nav__link nav__link--icon" data-modal="contact-form">Github</a>
+                </li> --}}
+            </ul>
+        </div>
     </nav>
 </header>
