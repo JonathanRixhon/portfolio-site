69421e38f5e72c6d2c6743932f201c52cd3f18d3
Jonathan Rixhon <jonathan@whitecube.be>
2024-01-05 23:39:04 +0100

    fix contact

--- resources/views/components/contact.blade.php
+++ resources/views/components/contact.blade.php
@@ -1,7 +1,7 @@
-<section class="contact wrapper">
-    <div class="contact__container">
-        <h2 class="contact__title">{{ $content['title'] }}</h2>
-        <div class="contact__body wysiwyg">
+<section class="contact-layout wrapper">
+    <div class="contact-layout__container">
+        <h2 class="contact-layout__title">{{ $content['title'] }}</h2>
+        <div class="contact-layout__body wysiwyg">
             {{ $content['body'] }}
         </div>
         <a href="{{ route('contact') }}" class="button button--big" data-modal="contact-form">{{ $content['cta'] }}</a>
